759c5886eed9029fc2e35440103f23ccd7e64517
Matheus Damazio <borisvonclaus@outlook.com.br>
2023-07-05 00:52:45 -0300

    Seção de Habilidades e responsividade mobile

--- static/script.js
+++ static/script.js
@@ -10,3 +10,16 @@ menuMobile.addEventListener('click', ()=> {
 
     body.classList.toggle("menu-nav-active")
 });
+
+/* Fechar o Menu ao clicar em um item mudando o ícone para list */
+
+const navItem = document.querySelectorAll('.nav-item');
+
+navItem.forEach(item =>{
+    item.addEventListener("click", ()=>{
+        if (body.classList.contains("menu-nav-active")){
+            body.classList.remove("menu-nav-active");
+            menuMobile.classList.replace("bi-x", "bi-list");
+        }
+    })
+})
